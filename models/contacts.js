430ddf9f8ed0078b2b1ab2bcc000a92057efc78b
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-07-19 13:40:17 +0300

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -12,7 +12,7 @@ const listContacts = async () => {
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
   const result = contacts.find(
-    (item) => item.id === contactId || item.contactId === contactId
+    (item) => item.id === contactId 
   );
   return result || null;
 };
@@ -20,7 +20,7 @@ const getContactById = async (contactId) => {
 const removeContact = async (contactId) => {
   const contacts = await listContacts();
   const index = contacts.findIndex(
-    (item) => item.id === contactId || item.contactId === contactId
+    (item) => item.id === contactId 
   );
   if (index === -1) {
     return null;
